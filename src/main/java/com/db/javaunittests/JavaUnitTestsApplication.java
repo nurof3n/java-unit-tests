494173670e0e6ea7d17a09eb99d354a6e400b3a2
Cioc Andrei <andy.cioc@yahoo.ro>
2022-04-21 12:21:55 +0300

    Initial commit

---
+++ src/main/java/com/db/javaunittests/JavaUnitTestsApplication.java
@@ -0,0 +1,13 @@
+package com.db.javaunittests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUnitTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaUnitTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/db/javaunittests/JavaUnitTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.javaunittests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUnitTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
